61719a8f91d19a9cc7181dea1371c987b5371552
=abhishektanwar <abhishektanwar084@gmail.com>
2022-03-12 07:14:28 +0530

    project initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Login from './Screens/auth/Login';
+import Home from './Screens/Home';
+import Register from './Screens/auth/Register'
+import {Switch,Route} from 'react-router-dom'
+import Header from './Components/Navbar/Header';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+		<Header />
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/login" exact component={Login} />
+			<Route path="/register" exact component={Register} />
+		</Switch>
+		</>
+	);
 }
-
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"
+  rel="stylesheet"
+/>
 export default App;
